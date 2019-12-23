4785c8c2033c05e4901546f10e06146cdbe6185f
vizeit <jigparekh@gmail.com>
2019-12-23 16:34:04 -0500

    Initial

--- LinkedStack.py
+++ LinkedStack.py
@@ -2,43 +2,57 @@ class Empty(Exception):
     """ Custom exception class for Empty Stack """
     pass
 
-class ArrayStack:
+class LinkedStack:
     """
-    LIFO (Last In First Out) implementation using python List
+    LIFO (Last In First Out) implementation using python Linked List
     """
+
+    class _node:
+        """ non-public class for Node of Linked List """
+        __slots__ = '_element', '_next' #streamline memory
+
+        def __init__(self, e, next):
+            self._element = e
+            self._next = next
+
     def __init__(self):
         """ Create and empty Stack """
-        self._data = [] #non-public empty list
+        self._head = None
+        self._size = 0
     
     def __len__(self):
         """ Return number of elements in Stack """
-        return len(self._data)
+        return self._size
 
     def is_empty(self):
         """ Return True if Stack is empty """
-        return len(self._data) == 0
+        return self._size == 0
 
     def push(self, e):
         """ Add element to Stack """
-        self._data.append(e) #New element added at the end
+        self._head = self._node(e, self._head)
+        self._size += 1
     
     def top(self):
         """ Return but do not remove top element from Stack """
         if not self.is_empty():
-            return self._data[-1]
+            return self._head._element
         else:
             raise Empty('Stack is empty') #Raise an excepton if Stack is empty
     
     def pop(self):
         """ Return and remove last element from Stack """
         if not self.is_empty():
-            return self._data.pop()
+            e = self._head._element
+            self._head = self._head._next
+            self._size -= 1
+            return e
         else:
             raise Empty('Stack is empty') #Raise an excepton if Stack is empty
 
 if __name__ == "__main__":
     try:
-        a = ArrayStack()
+        a = LinkedStack()
         a.push(1)
         a.push('a')
         a.push(1.2)
