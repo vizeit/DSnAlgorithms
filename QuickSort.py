eba0f52dc5c70d28aa7c006372981f6bc0944d51
vizeit <jigparekh@gmail.com>
2020-06-12 22:06:42 -0400

    Initial

---
+++ QuickSort.py
@@ -0,0 +1,42 @@
+def partition(l, lo, hi):
+    p = l[lo]
+    i = lo
+    j = hi+1
+    while 1:
+        i += 1
+        while l[i] < p and l[i] != p:
+            if i == hi:
+                break
+            i += 1
+        j -= 1
+        while l[j] > p and l[j] != p:
+            if j == lo:
+                break
+            j -= 1
+        if i >= j:
+            break
+        l[i], l[j] = l[j], l[i]
+    l[lo], l[j] = l[j], p
+    return j
+
+
+def _quicksort(l, lo, hi):
+    if hi <= lo:
+        return
+    ip = partition(l, lo, hi)
+
+    _quicksort(l, lo, ip-1)
+    _quicksort(l, ip+1, hi)
+
+def quicksort(l):
+    _quicksort(l, 0, len(l)-1)
+
+if __name__ == "__main__":
+    import random
+    rn = 5000
+    l = []
+    for i in range(rn):
+        l.append(random.randrange(rn))
+    quicksort(l)
+    print(l)
+    
