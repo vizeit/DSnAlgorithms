ce02104fa74f2364b79b4a57e8718d7cf494d35f
vizeit <jigparekh@gmail.com>
2020-06-16 15:05:34 -0400

    added DFS & BFS

--- Graph.py
+++ Graph.py
@@ -1,3 +1,5 @@
+import LinkedQueue
+
 class Graph:
     class vertex:
         __slots__ = '_element'
@@ -61,6 +63,28 @@ class Graph:
         edge = self.edge(u, v, e)
         self._outgoing[u][v] = edge
         self._incoming[v][u] = edge
+    def dfs(self, u, discovered):#Depth First Search
+        """
+        u - starting vertex initialized in discovered dictionary 
+        """
+        for e in self.incident_edges(u):
+            v = e.opposite(u)
+            if v not in discovered:
+                discovered[v] = e
+                self.dfs(v,discovered)
+    def bfs(self, u, discovered):#Breadth first search
+        """
+        u - starting vertex initialized int discovered dictionary
+        """
+        q = LinkedQueue.LinkedQueue()
+        q.put(u)
+        while not q.is_empty():
+            s = q.get()
+            for e in self.incident_edges(s):
+                v = e.opposite(s)
+                if v not in discovered:
+                    discovered[v] = e
+                    q.put(v)
 
 if __name__ == "__main__":
     g = Graph()
@@ -78,3 +102,10 @@ if __name__ == "__main__":
     g.insert_edge(v3,v2)
     print([v.element() for v in g.vertices()])
     print([(e.endpoints()[0].element(),e.endpoints()[1].element()) for e in g.edges()])
+    ddfs = {v0:None}
+    g.dfs(v0, ddfs)
+    print([(i.element(), (j.endpoints()[0].element(),j.endpoints()[1].element()) if j else None) for i,j in ddfs.items()])
+
+    dbfs = {v0:None}
+    g.bfs(v0, dbfs)
+    print([(i.element(), (j.endpoints()[0].element(),j.endpoints()[1].element()) if j else None) for i,j in dbfs.items()])
