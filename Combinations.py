bc2bc74875a9e6bc17d3d7cb5be64a2f13a01979
vizeit <jigparekh@gmail.com>
2020-07-19 21:30:07 -0400

    refactor

--- Combinations.py
+++ Combinations.py
@@ -1,18 +1,16 @@
-def combinations(ls, r, rs, s=[]):
+def _subcombinations(ls, r, rs, s=[]):
     for i in range(len(ls)):
-        if len(s) + (len(ls)-i) < r:#avoid any extra cycles
-            return
+        if len(s) + (len(ls)-i) < r: return #avoid any extra cycles
         s.append(ls[i])
-        if len(s) == r:
-            rs.append(''.join(s))
-            s.pop()
-        else:
-            combinations(ls[i+1:], r, rs, s)
-            s.pop()
+        rs.append(''.join(s)) if len(s) == r else _subcombinations(ls[i+1:], r, rs, s)
+        s.pop()
+
+def combinations(s):
+    rs = []
+    l = list(s)
+    for i in range(1,len(l)+1):
+        _subcombinations(l,i,rs)
+    return rs
 
 if __name__ == "__main__":
-    rs = []
-    l = list('abcde')
-    for i in range(1,len(l)+1):
-        combinations(l,i,rs)
-    print(rs)
+    print(combinations('abc'))
--- Permutations.py
+++ Permutations.py
@@ -1,15 +1,14 @@
-def _subpermutations(k, s, u, rs):
+def _subpermutations(u, r, rs, s=[]):
     for i in range(len(u)):
         s.append(u.pop(i))
-        if k == 1:
-            rs.append(''.join(s))
-        else:
-            _subpermutations(k-1, s, u,rs)
+        rs.append(''.join(s)) if r == 1 else _subpermutations(u, r-1, rs, s)
         u.insert(i,s.pop())#backtracking
 
 def permutations(s):
     rs = []
-    _subpermutations(len(s), [], list(s),rs)
+    l = list(s)
+    for i in range(1, len(l)+1):
+        _subpermutations(l, i, rs)
     return rs
 
 if __name__ == "__main__":
