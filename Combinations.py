9fd1d6063aa07f8f4e244500101d4dc2139a9dab
vizeit <jigparekh@gmail.com>
2020-07-22 23:06:47 -0400

    refactor

--- Combinations.py
+++ Combinations.py
@@ -1,8 +1,11 @@
 def _subcombinations(ls, r, s=[]):
     for i in range(len(ls)):
+        if len(s) + (len(ls)-i) < r: break #avoid any extra cycles
         s.append(ls[i])
-        if len(s) == r: yield ''.join(s) 
-        yield from _subcombinations(ls[i+1:], r, s)
+        if len(s) == r: 
+            yield ''.join(s)
+        else: 
+            yield from _subcombinations(ls[i+1:], r, s)
         s.pop()
 
 def combinations(s):
--- Permutations.py
+++ Permutations.py
@@ -1,8 +1,10 @@
 def _subpermutations(u, r, s=[]):
     for i in range(len(u)):
         s.append(u.pop(i))
-        if r == 1: yield ''.join(s) 
-        yield from _subpermutations(u, r-1, s)
+        if r == 1: 
+            yield ''.join(s)
+        else: 
+            yield from _subpermutations(u, r-1, s)
         u.insert(i,s.pop())#backtracking
 
 def permutations(s):
